08198453d35cb07c8972d85b82e316d33754711b
Yehonal <hw.2@hotmail.it>
2013-11-11 16:23:20 +0100

    Removed UI error messages and optimized code

--- AddOns/Udw_LootTools/Loot.lua
+++ AddOns/Udw_LootTools/Loot.lua
@@ -1,32 +1,30 @@
 local UdwItemsInBag = {}
 local UdwDestroyItems = {}
-local autoDestroyRun=false
-local lootClosed=false
 local updateDiff = 0
+local dCount = 0
+local lootClosed=false
 
-local function eventHandler(self, event, ...)
-	if (autoDestroyRun==true) then
-		if (event=="BAG_UPDATE" or event=="UDW_AUTOLOOT_START") then
-			Udw_AutoLoot();
-		end
-		
+local function eventHandler(self, event, message)
+	if (dCount>0) then
 		if (event=="LOOT_CLOSED") then
 			lootClosed=true
+		else
+			Udw_AutoLoot();
 		end
 	end
 end
 
-local function onUpdate(self,elapsed) 
-	if (autoDestroyRun==true) then
+local function onUpdate(self,elapsed)   
+	if (dCount>0) then	
 		Udw_AutoDestroy();
 	end
-   
-	if (lootClosed==true) then
+
+	if (dCount==0 and lootClosed==true) then
 		updateDiff = updateDiff + elapsed;         
 		if (updateDiff>1) then
-				autoDestroyRun=false;
-				lootClosed=false
-				updateDiff = 0;
+			UIErrorsFrame:RegisterEvent("UI_ERROR_MESSAGE")
+			lootClosed=false
+			updateDiff = 0;
 		end
 	end
 end
@@ -35,42 +33,65 @@ function Udw_SlashCommand(msg)
   if(msg) then
 	local command = strlower(msg)
 	if (command == "destroy") then
-		UdwDestroyItems = {}
-		if not Udw_GetBagItems() then
-			UIErrorsFrame:AddMessage("Free at least one space in your bag to destroy loot!", 1.0, 1.0, 1.0, 1.0, UIERRORS_HOLD_TIME)
-		else
-			autoDestroyRun=true;
-			eventHandler(this,"UDW_AUTOLOOT_START");
-		end
+		Udw_DestroyStart()
 	end
   end
 end
 
+function Udw_DestroyStart() 
+	if (GetNumLootItems()>0) then
+		UdwDestroyItems = {}
+		freeSlots=Udw_TotalFreeSlots()
+		if freeSlots==0 then
+			UIErrorsFrame:AddMessage("Free at least one space in your bag to destroy loot!", 1.0, 1.0, 1.0, 1.0, UIERRORS_HOLD_TIME)
+		else
+			Udw_GetBagItems()
+			Udw_GetLootItems()
+			UIErrorsFrame:UnregisterEvent("UI_ERROR_MESSAGE")
+			eventHandler(this,"UDW_DESTROY_START")
+		end
+	end
+end
+
 function Udw_AutoLoot()
-	local numItems = GetNumLootItems()
-	--DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Udw number of items:" .. numItems)
+	local numItems=GetNumLootItems()
 	for i=1,numItems do
 		local itemLink=GetLootSlotLink(i)
 		if (itemLink) then
 			LootSlot(i)
 			ConfirmLootSlot(i)
-			local itemId = string.match (itemLink, "item:(%d+)")
-			UdwDestroyItems[#(UdwDestroyItems) + 1] = tonumber(itemId)
+		end
+	end
+end
+
+function Udw_AutoDestroy()
+	for i = 0, 4, 1 do
+		local x = GetContainerNumSlots(i)
+		for j = 0, x, 1 do
+			local cItemId=GetContainerItemID(i, j)
+			if cItemId then
+				cItemId=tonumber(cItemId)
+				if UdwItemsInBag[i][j] == false and dCount>0 and UdwDestroyItems[cItemId] == true then
+					PickupContainerItem(i, j)
+					if CursorHasItem() then
+						--local sName, sLink, iRarity, iLevel, iMinLevel, sType, sSubType, iStackCount = GetItemInfo(cItemId);
+						--DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Udw Destroying ".. sName)
+						UdwDestroyItems[cItemId]=false
+						dCount=dCount-1
+						DeleteCursorItem()
+					end
+				end
+			end
 		end
 	end
 end
 
 function Udw_GetBagItems() 
 	UdwItemsInBag = {}
-	local freeSpace=false
+	
 	for i = 0, 4, 1 do
 		UdwItemsInBag[i]={}
-		x = GetContainerNumSlots(i)
-		
-		local freeSlots, bagType = GetContainerNumFreeSlots(i)
-		if ( freeSlots>0 ) then
-			freeSpace=true
-		end
+		local x = GetContainerNumSlots(i)
 		
 		for j = 0, x, 1 do
 			if GetContainerItemID(i, j) then
@@ -80,41 +101,35 @@ function Udw_GetBagItems()
 			end
 		end
 	end
-	
-	return freeSpace
 end
 
-function Udw_AutoDestroy()
-	for i = 0, 4, 1 do
-		x = GetContainerNumSlots(i)
-		for j = 0, x, 1 do
-			local cItemId=GetContainerItemID(i, j)
-			if cItemId then
-				if UdwItemsInBag[i][j] == false then
-					PickupContainerItem(i, j)
-					if CursorHasItem() and Udw_InTable(UdwDestroyItems,cItemId) then
-						--DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Udw Destroying ".. cItemId)
-						DeleteCursorItem()
-					end
+function Udw_GetLootItems()
+	local numItems=GetNumLootItems()
+	if (numItems>0) then
+		for i=1,numItems do
+			local itemLink=GetLootSlotLink(i)
+			if (itemLink) then
+				local itemId = string.match (itemLink, "item:(%d+)")
+				if (itemId) then
+					itemId=tonumber(itemId)
+					UdwDestroyItems[itemId] = true
+					dCount=dCount+1
 				end
 			end
 		end
+		
 	end
 end
 
-function Udw_InTable(t, val)
-	for index, v in ipairs(t) do
-		local localValue
-		if(type(v) == "table") then
-			localValue = v.name
-		else
-			localValue = v
+function Udw_TotalFreeSlots()
+	local totalFreeNum=0
+	for i = 0, 4, 1 do
+		local freeSlots, bagType = GetContainerNumFreeSlots(i)
+		if ( freeSlots>0 ) then
+			totalFreeNum=totalFreeNum+freeSlots
 		end
-  		if localValue == val then
-		    return index
-  		end
 	end
-	return false
+	return totalFreeNum
 end
 
 -- Addon Loading
@@ -135,7 +150,8 @@ if( DEFAULT_CHAT_FRAME ) then
 end
 
 frame:RegisterEvent("BAG_UPDATE")
-frame:RegisterEvent("LOOT_SLOT_CLEARED")
-frame:RegisterEvent("LOOT_SLOT_CHANGED")
-frame:RegisterEvent("LOOT_CLOSED")
-	
+--frame:RegisterEvent("LOOT_SLOT_CLEARED")
+--frame:RegisterEvent("LOOT_SLOT_CHANGED")
+--frame:RegisterEvent("LOOT_CLOSED")
+--frame:RegisterEvent("CHAT_MSG_LOOT")
+--frame:RegisterEvent("UI_ERROR_MESSAGE")
